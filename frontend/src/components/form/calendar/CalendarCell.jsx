548d9dd272bf6977f48e554cd00c360e51abe75b
studzik21 <pstudzik@student.agh.edu.pl>
2022-04-11 21:56:25 +0200

    Add Calendar components

---
+++ frontend/src/components/form/calendar/CalendarCell.jsx
@@ -0,0 +1,31 @@
+/**
+ *Creates CalendarCell
+ * @memberof CalendarLabel and Calendar
+ * 
+ * @param id int,slot number
+ * @param label string,displayed label
+ * @param onClick function to handle cliclikng on the slot
+ * @param isAvailable bool, true if term is available 
+ * @param isChosen bool, true if student chose this term
+ * @param isLabel bool, true if Cell is CalendarLabel
+ * 
+ * @example <CalendarCell id = {1} onClick= {()=>console.log(id), isAvailabe ={true}, isChosen ={false} isLabel={false}}
+ */
+
+
+ function CalendarCell({ id,label,onClick,isAvailable,isChosen,isLabel}) {
+    let color = "DimGray";
+    if (isAvailable) color = "LightGrey";
+    if (isChosen) color ="LightGreen";
+    if(isLabel) color="orange";
+
+    const cellStyle={ 
+        padding: "0.5em 0",
+        backgroundColor: color,
+        border: "0.5px solid"
+    }
+
+    return (<td className="calendar-cell" style={cellStyle} onClick ={isAvailable? ()=>onClick(id): undefined }>{label}</td>);
+}
+
+export default CalendarCell; 
