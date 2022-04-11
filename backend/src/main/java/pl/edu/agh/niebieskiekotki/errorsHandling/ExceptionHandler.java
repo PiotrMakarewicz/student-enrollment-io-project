c12496403b72c0194d9accd9b26edd55e358619e
Szymon Sumara <szymon.sumara.zst@gmail.com>
2022-04-11 14:42:27 +0200

    CRUD scelecton

---
+++ backend/src/main/java/pl/edu/agh/niebieskiekotki/errorsHandling/ExceptionHandler.java
@@ -0,0 +1,19 @@
+package pl.edu.agh.niebieskiekotki.errorsHandling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import pl.edu.agh.niebieskiekotki.errorsHandling.exceptions.NotFoundException;
+
+import java.io.IOException;
+
+@ControllerAdvice
+public class ExceptionHandler extends ResponseEntityExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorMessage> HandleNotFound(NotFoundException exception) throws IOException{
+
+        ErrorMessage errorMessage = new ErrorMessage( HttpStatus.NOT_FOUND, exception.getMessage());
+        return new ResponseEntity(errorMessage, HttpStatus.NOT_FOUND);
+    }
+}
---
+++ backend/src/main/java/pl/edu/agh/niebieskiekotki/errorsHandling/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package pl.edu.agh.niebieskiekotki.errorsHandling.exceptions;
+
+public class NotFoundException extends Exception {
+    public NotFoundException(String message) {
+        super(  message);
+    }
+}
