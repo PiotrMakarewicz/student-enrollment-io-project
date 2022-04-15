13d2465e23c0f2db871e4fda99804baeb1dce202
dkawalecc <dkawalec08@gmail.com>
2022-04-15 08:40:24 +0200

    NK-56 fix + Form wrapper added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,30 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import LecturerForm from "./components/form/LecturerForm";
+import LecturerForm from "./components/form/LecturerForm/LecturerForm";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <div className="App">
-          <header className="App-header py-2 px-3">
-            <Link to="/">Strona główna</Link>
-            <ul>
-              <li>
-                <Link to="/new-form">Nowa ankieta</Link>
-              </li>
-              <li>
-                <Link to="/my-forms">Twoje ankiety</Link>
-              </li>
-              <li>
-                <Link to="/settings">Ustawienia</Link>
-              </li>
-            </ul>
-          </header>
-          <div className="App-body">
-            <Routes>
-              <Route path="/new-form" element={<LecturerForm />} />
-              <Route path="*" element={<h2>Page not found</h2>} />
-            </Routes>
-          </div>
-        </div>
-      </Router>
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <div className="App">
+                    <Navbar />
+                    <div className="App-body">
+                        <Routes>
+                            <Route
+                                path="/new-form"
+                                element={<LecturerForm />}
+                            />
+                            <Route
+                                path="*"
+                                element={<h2>Page not found</h2>}
+                            />
+                        </Routes>
+                    </div>
+                </div>
+            </Router>
+        </>
+    );
 }
 
 export default App;
